216e8ec64205181907ff17fb16f8ec01e6a40681
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2024-06-05 15:51:31 +0300

    I bet there is a smarter way to do this

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import store from './store';
-import axios from 'axios';
-import App from './App';
+import store from "./store";
+import axios from "axios";
+import App from "./App";
 
 const location =
-  window.location.href === 'http://localhost:4000/'
-    ? 'http://localhost:4000/'
-    : 'https://curved-maze-mole.glitch.me/';
+  window.location.href === "http://localhost:4000/"
+    ? "http://localhost:4000/"
+    : "https://dc-shopping-list-api.onrender.com/";
 axios.defaults.baseURL = location;
 
 ReactDOM.render(
@@ -19,5 +19,5 @@ ReactDOM.render(
     </React.StrictMode>
     ,
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
